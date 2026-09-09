40f3cd7daee24cedd37cab0bcaee62aee74a4f19
Hai-raat <hairaatyakub@gmail.com>
2026-09-09 11:16:37 +0000

    feat: add handleRemoveService function

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,21 +1,31 @@
-import {useState} from 'react'
-import { services } from '../data'
-import Title from './Title'
-import Service from './Service'
+import { useState } from "react";
+import { services } from "../data";
+import Title from "./Title";
+import Service from "./Service";
 
 function Services() {
-  const [servicesData, setServicesData] = useState(services)
+  const [servicesData, setServicesData] = useState(services);
+
+  const handleRemoveService = (id) => {
+    setServicesData(servicesData.filter((service) => service.id !== id));
+  };
 
   return (
-    <section className='section services' id='services'>
-      <Title title='our' subTitle='services' />
+    <section className="section services" id="services">
+      <Title title="our" subTitle="services" />
 
-      <div className='section-center services-center'>
+      <div className="section-center services-center">
         {servicesData.map((service) => {
-          return <Service {...service} key={service.id} />
+          return (
+            <Service
+              {...service}
+              key={service.id}
+              onRemove={handleRemoveService}
+            />
+          );
         })}
       </div>
     </section>
-  )
+  );
 }
-export default Services
+export default Services;
